1d67daf09195e5ecaac1372587bbefc3f76b19eb
Juan Larosa <juanlarosa96@gmail.com>
2016-04-27 18:29:19 -0300

    Agrego socket cliente y envio mensaje a Nucleo

--- Consola/src/Consola.c
+++ Consola/src/Consola.c
@@ -1,19 +1,30 @@
-/*
- ============================================================================
- Name        : Consola.c
- Author      : Dieta
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+static const int PUERTO_NUCLEO = 8080;
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
 
-	//cliente de nucleo
-}
+	struct sockaddr_in direccionServidor;
+
+	direccionServidor.sin_family = AF_INET;
+	direccionServidor.sin_addr.s_addr = inet_addr("127.0.0.1");
+	direccionServidor.sin_port = htons(PUERTO_NUCLEO);
+
+	int cliente = socket(AF_INET, SOCK_STREAM, 0);
+
+
+	if (connect(cliente, (void*) &direccionServidor, sizeof(direccionServidor)) != 0) {
+		perror("No se pudo conectar");
+		return 1;
+	}
+		char mensaje[1000];
+		scanf("%s", mensaje);
+		send(cliente, mensaje, strlen(mensaje), 0);
+
+
+		return EXIT_SUCCESS;
+	}
