d9ec6812d277d15644135643552298ba9e59e980
Fatta <lfatta2@hotmail.com>
2016-04-29 12:08:10 -0300

    Agrego socket servidor en Swap

--- Swap/src/Swap.c
+++ Swap/src/Swap.c
@@ -7,13 +7,57 @@
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include<commons/config.h>
+
+static const int PUERTO_SERVIDOR_SWAP = 9001;
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+	struct sockaddr_in direccionServidorSwap;
+	direccionServidorSwap.sin_family = AF_INET;
+	direccionServidorSwap.sin_addr.s_addr = INADDR_ANY;
+	direccionServidorSwap.sin_port = htons(PUERTO_SERVIDOR_SWAP);
 
-	//servidor de umc
+	int servidorSwap = socket(AF_INET, SOCK_STREAM, 0);
+
+	int activado = 1;
+	setsockopt(servidorSwap, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
+
+	if (bind(servidorSwap, (void*) &direccionServidorSwap, sizeof(direccionServidorSwap)) != 0) {
+		perror("Falló el bind");
+		return 1;
+	}
+
+	printf("Estoy escuchando\n");
+	listen(servidorSwap, 100);
+
+	//------------------------------
+
+	struct sockaddr_in direccionCliente;
+	unsigned int len;
+	len = sizeof(struct sockaddr_in);
+
+	int cliente = accept(servidorSwap, (void*) &direccionCliente, &len);
+	printf("Recibí una conexión en %d!!\n", cliente);
+
+	//------------------------------
+
+	char* bufferServidor = malloc(100);
+
+	int bytesRecibidos = recv(cliente, bufferServidor, 100, 0);
+	if (bytesRecibidos < 0) {
+		perror("Se Desconecto");
+		return 1;
+	}
+
+	bufferServidor[bytesRecibidos] = '\0';
+	printf("Me llegaron %d bytes con %s", bytesRecibidos, bufferServidor);
+
+	free(bufferServidor);
+
+	return 0;
 }
