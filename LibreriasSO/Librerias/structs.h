f9cc964da3b4e022fee60c9ca33d0021ebcfa75d
Julian Saidman <julian.saidman@gmail.com>
2016-05-14 21:27:44 -0300

    Termino pcb en la shared library

--- LibreriasSO/Librerias/structs.h
+++ LibreriasSO/Librerias/structs.h
@@ -7,19 +7,43 @@
 
 #ifndef LIBRERIAS_STRUCTS_H_
 #define LIBRERIAS_STRUCTS_H_
+#include <"commons/collections/list.h">
+#include <"commons/collections/dictionary.h">
+#include <"parser/metadata_program.h">
 
-typedef struct pcb {
+typedef struct {
+	uint32_t inicio;
+	uint32_t longitud;
+}t_posicion_instruccion;
+
+typedef struct {
+	uint32_t pagina;
+	uint32_t offset;
+	uint32_t size;
+}t_posicion_memoria;
+
+
+typedef struct {
+	t_list lista_argumentos;
+	t_list lista_variables;
+	uint32_t direccion_retorno;
+	t_posicion_memoria variable_retorno
+} t_registro_pila;
+
+typedef struct {
+	t_registro_pila registro;
+	t_pila indice_stack;
+} t_pila;
+
+typedef struct {
 	uint32_t pid;
 	uint32_t pc;
 	uint32_t paginas_codigo;
-	uint32_t indice_codigo;
-	uint32_t indice_etiquetas;
+	t_posicion_instruccion indice_codigo;
+	t_metadata_program indice_etiquetas;
 	uint32_t indice_stack;
 } t_pcb;
 
-typedef struct posicion_instruccion {
-	uint32_t inicio;
-	uint32_t longitud;
-};
+
 
 #endif /* LIBRERIAS_STRUCTS_H_ */
