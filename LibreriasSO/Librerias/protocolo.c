830d34d5aad2e7315dea8697a074169619a6943d
Juan Larosa <juanlarosa96@gmail.com>
2016-05-16 01:32:08 -0300

    Modifico recibirHeader en libreria protocolo para que devuelva bytesRecibidos en caso de error en el recv

--- LibreriasSO/Librerias/protocolo.c
+++ LibreriasSO/Librerias/protocolo.c
@@ -6,21 +6,25 @@
  */
 #include "protocolo.h"
 
-int recibirHeader(int socketOrigen){
+int recibirHeader(int socketOrigen) {
 	int header;
-	recv(socketOrigen, &header,sizeof(int),0);
-	return header;
+	int bytesRecibidos;
+	if ((bytesRecibidos = recv(socketOrigen, &header, sizeof(int), 0)) <= 0) {
+		return bytesRecibidos;
+	} else {
+		return header;
+	}
 }
 
-void enviarProgramaAnsisop(int socketDestino, char * codigo, int largoCodigo){
+void enviarProgramaAnsisop(int socketDestino, char * codigo, int largoCodigo) {
 	int header = programaAnsisop;
-	send(socketDestino,&header,sizeof(int),0);
-	send(socketDestino,&largoCodigo,sizeof(int),0);
-	send(socketDestino,codigo,largoCodigo,0);
+	send(socketDestino, &header, sizeof(int), 0);
+	send(socketDestino, &largoCodigo, sizeof(int), 0);
+	send(socketDestino, codigo, largoCodigo, 0);
 }
 
-void recibirProgramaAnsisop(int socketOrigen, char * codigo){
+void recibirProgramaAnsisop(int socketOrigen, char * codigo) {
 	int largoCodigo;
-	recibirTodo(socketOrigen,&largoCodigo,sizeof(int));
-	recibirTodo(socketOrigen,codigo,&largoCodigo);
+	recibirTodo(socketOrigen, &largoCodigo, sizeof(int));
+	recibirTodo(socketOrigen, codigo, &largoCodigo);
 }
