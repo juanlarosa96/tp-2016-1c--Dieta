1cd528ff9e70b829be4bdfedb1e35e60fc011d39
Sofia Cortes <sofiavcortes@gmail.com>
2016-05-19 22:36:36 -0300

    Firma para operaciones de UMC

---
+++ UMC/src/funcionesUMC.h
@@ -0,0 +1,26 @@
+/*
+ * funcionesUMC.h
+ *
+ *  Created on: 19/5/2016
+ *      Author: utnso
+ */
+
+#ifndef FUNCIONESUMC_H_
+#define FUNCIONESUMC_H_
+
+
+/*-------OperatoriaUMC-------*/
+
+void inicializarPrograma(uint32_t idPrograma, int paginasRequeridas);
+
+void solicitarBytesDeUnaPag(int nroPagina, int offset, int tamanio);
+
+void guardarBytesEnUnaPag(int nroPagina, int offset, int tamanio, void * buffer);
+
+void finalizarPrograma(uint32_t idPrograma);
+
+void cambioProceso(uint32_t idPrograma);
+
+
+
+#endif /* FUNCIONESUMC_H_ */
