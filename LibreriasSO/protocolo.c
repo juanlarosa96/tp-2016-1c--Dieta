4d1f38b07950980dce514434f267ea0aa8c9a2f1
Sofia Cortes <sofiavcortes@gmail.com>
2016-05-20 18:07:33 -0300

    Creo proyecto de LibreriasSO

---
+++ LibreriasSO/protocolo.c
@@ -0,0 +1,35 @@
+/*
+ * protocolo.c
+ *
+ *  Created on: 15/5/2016
+ *      Author: utnso
+ */
+#include "protocolo.h"
+
+int recibirHeader(int socketOrigen) {
+	int header;
+	int bytesRecibidos;
+	if ((bytesRecibidos = recv(socketOrigen, &header, sizeof(int), 0)) <= 0) {
+		return bytesRecibidos;
+	} else {
+		return header;
+	}
+}
+
+void enviarProgramaAnsisop(int socketDestino, char * codigo, int largoCodigo) {
+	int header = programaAnsisop;
+	send(socketDestino, &header, sizeof(int), 0);
+	send(socketDestino, &largoCodigo, sizeof(int), 0);
+	send(socketDestino, codigo, largoCodigo, 0);
+}
+
+void recibirProgramaAnsisop(int socketOrigen, char * codigo, int largoCodigo) {
+
+	recibirTodo(socketOrigen, codigo, &largoCodigo);
+}
+int recibirLargoProgramaAnsisop(int socketOrigen) {
+	int largoCodigo;
+	recibirTodo(socketOrigen, &largoCodigo, sizeof(int));
+	return largoCodigo;
+
+}
---
+++ LibreriasSO/structs.h
@@ -0,0 +1,49 @@
+/*
+ * structs.h
+ *
+ *  Created on: 14/5/2016
+ *      Author: utnso
+ */
+
+#ifndef LIBRERIAS_STRUCTS_H_
+#define LIBRERIAS_STRUCTS_H_
+#include <commons/collections/list.h>
+#include <commons/collections/dictionary.h>
+#include <parser/metadata_program.h>
+
+typedef struct {
+	uint32_t inicio;
+	uint32_t longitud;
+}t_posicion_instruccion;
+
+typedef struct {
+	uint32_t pagina;
+	uint32_t offset;
+	uint32_t size;
+}t_posicion_memoria;
+
+
+typedef struct {
+	t_list lista_argumentos;
+	t_list lista_variables;
+	uint32_t direccion_retorno;
+	t_posicion_memoria variable_retorno;
+} t_registro_pila;
+
+typedef struct t_pila{
+	t_registro_pila registro;
+	struct t_pila * indice_stack;
+} t_pila;
+
+typedef struct {
+	uint32_t pid;
+	uint32_t pc;
+	uint32_t paginas_codigo;
+	t_posicion_instruccion indice_codigo;
+	t_metadata_program indice_etiquetas;
+	uint32_t indice_stack;
+} t_pcb;
+
+
+
+#endif /* LIBRERIAS_STRUCTS_H_ */
