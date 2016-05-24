81477820de17cc7932329bb8615c160988d83c27
Juan Larosa <juanlarosa96@gmail.com>
2016-05-24 13:06:55 -0300

    Modifico el struct del pcb

--- LibreriasSO/structs.h
+++ LibreriasSO/structs.h
@@ -39,9 +39,9 @@ typedef struct {
 	uint32_t pid;
 	uint32_t pc;
 	uint32_t paginas_codigo;
-	t_posicion_instruccion indice_codigo;
+	t_intructions * indice_codigo;
 	t_metadata_program indice_etiquetas;
-	uint32_t indice_stack;
+	t_pila indice_stack;
 } t_pcb;
 
 
