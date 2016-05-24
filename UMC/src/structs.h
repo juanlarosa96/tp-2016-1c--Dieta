e09c4a7643b2502e651e1f28c51e02c084292a31
Sofia Cortes <sofiavcortes@gmail.com>
2016-05-24 15:55:33 -0300

    Agrego structs para UMC

---
+++ UMC/src/structs.h
@@ -0,0 +1,40 @@
+/*
+ * structs.h
+ *
+ *  Created on: 24/5/2016
+ *      Author: utnso
+ */
+
+#ifndef STRUCTS_H_
+#define STRUCTS_H_
+
+struct nodo_lista_frames {
+	uint32_t pid;
+	uint8_t bitReferencia;
+	uint8_t bitModificado;
+	uint8_t paginaModificada;
+};
+
+typedef struct nodo_lista_frames t_nodo_lista_frames;
+
+struct nodo_lista_paginas {
+	int nro_pagina; //?
+	char status; // M = memoria, S = SWAP
+};
+
+typedef struct nodo_lista_paginas t_nodo_lista_paginas;
+
+struct nodo_lista_procesos {
+	uint32_t pid;
+	uint8_t tamanioProceso;
+	//uint8_t paginasAsignadas;
+	t_list * lista_paginas;
+
+};
+
+typedef struct nodo_lista_procesos t_nodo_lista_procesos;
+
+
+
+
+#endif /* STRUCTS_H_ */
