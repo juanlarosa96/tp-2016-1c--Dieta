3c4d6c775f46ed31fea4f0fa95df83d2faf99b7b
Juan Larosa <juanlarosa96@gmail.com>
2016-05-25 20:59:04 -0300

    Modifico funcion recibirTodo para que retorne 1 si se corta la conexion

--- LibreriasSO/sockets.c
+++ LibreriasSO/sockets.c
@@ -1,12 +1,12 @@
 #include "sockets.h"
 
 /*
-uint8_t IDCONSOLA = 1;
-uint8_t IDNUCLEO = 2;
-uint8_t IDCPU = 3;
-uint8_t IDUMC = 4;
-uint8_t IDSWAP = 5;
-*/
+ uint8_t IDCONSOLA = 1;
+ uint8_t IDNUCLEO = 2;
+ uint8_t IDCPU = 3;
+ uint8_t IDUMC = 4;
+ uint8_t IDSWAP = 5;
+ */
 
 int escucharEn(int unSocket, int puerto) {
 
@@ -16,8 +16,7 @@ int escucharEn(int unSocket, int puerto) {
 	address.sin_port = htons(puerto);
 	//memset(&(address.sin_zero), '\0', 8); Veriguar si falta '\0'
 
-	if ((bind(unSocket, (struct sockaddr *) &address, sizeof(struct sockaddr)))
-			== 1) {
+	if ((bind(unSocket, (struct sockaddr *) &address, sizeof(struct sockaddr))) == 1) {
 		//No se puedo ejecutar bind en el socket
 		return 1;
 	}
@@ -38,8 +37,7 @@ int conectarA(int unSocket, char * ip, int puerto) {
 	address.sin_port = htons(puerto);
 	//memset(&(address.sin_zero), '\0', 8); Ver si le falta el '\0'
 
-	if (connect(unSocket, (struct sockaddr *) &address, sizeof(struct sockaddr))
-			!= 0) {
+	if (connect(unSocket, (struct sockaddr *) &address, sizeof(struct sockaddr)) != 0) {
 		//No se puedo conectar el socket
 		return 1;
 	}
@@ -55,8 +53,7 @@ int crearSocket(int *unSocket) {
 	} else {
 
 		int yes = 1;
-		if ((setsockopt(*unSocket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)))
-				== 1) {
+		if ((setsockopt(*unSocket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))) == 1) {
 			//Error seteando el socket
 			return 1;
 		}
@@ -70,37 +67,43 @@ int iniciarHandshake(int socketDestino, uint8_t idOrigen) {
 
 	send(socketDestino, &idOrigen, sizeof(uint8_t), 0);
 
-	recibirTodo(socketDestino,&idRecibido,sizeof(uint8_t));
+	recibirTodo(socketDestino, &idRecibido, sizeof(uint8_t));
 
 	return idRecibido;
 }
 
 int responderHandshake(int socketDestino, uint8_t idOrigen, uint8_t idEsperado) {
 	uint8_t idRecibido;
-	recibirTodo(socketDestino,&idRecibido,sizeof(uint8_t));
+	recibirTodo(socketDestino, &idRecibido, sizeof(uint8_t));
 
-		if (idRecibido != idEsperado){
-			send(socketDestino, IDERROR, sizeof(uint8_t), 0);
-			return 1;
-		}else{
-			send(socketDestino, &idOrigen, sizeof(uint8_t), 0);
-			return 0;
-		}
+	if (idRecibido != idEsperado) {
+		send(socketDestino, IDERROR, sizeof(uint8_t), 0);
+		return 1;
+	} else {
+		send(socketDestino, &idOrigen, sizeof(uint8_t), 0);
+		return 0;
+	}
 
 }
 
-int aceptarConexion(int socketServidor, struct sockaddr_in * direccionCliente){
+int aceptarConexion(int socketServidor, struct sockaddr_in * direccionCliente) {
 	unsigned int len;
 	len = sizeof(struct sockaddr_in);
 	return accept(socketServidor, (void*) &direccionCliente, &len);
 
 }
 
-void recibirTodo(int socketOrigen, void * buffer,int largo){
+int recibirTodo(int socketOrigen, void * buffer, int largo) {
 	int bytesRecibidos = 0;
-	bytesRecibidos = recv(socketOrigen, buffer, largo,0);
-	while (bytesRecibidos < largo) {
-		bytesRecibidos = bytesRecibidos - recv(socketOrigen, buffer + bytesRecibidos, sizeof(uint8_t) - bytesRecibidos,0);
-	}
+	int bytesRecibidosTotales = 0;
+	bytesRecibidos = recv(socketOrigen, buffer, largo, 0);
+	while (bytesRecibidosTotales < largo) {
 
+		bytesRecibidos = recv(socketOrigen, buffer + bytesRecibidos, sizeof(uint8_t) - bytesRecibidos, 0);
+		bytesRecibidosTotales = bytesRecibidosTotales + bytesRecibidos;
+		if (bytesRecibidos <= 0) {
+			return 1;
+		}
+	}
+	return 0;
 }
