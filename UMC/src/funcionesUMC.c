a45630845a591f64cd5a5234b45155024131afae
Sofia Cortes <sofiavcortes@gmail.com>
2016-05-25 15:30:12 -0300

    Funciones para hilos pero sin terminar en UMC

--- UMC/src/funcionesUMC.c
+++ UMC/src/funcionesUMC.c
@@ -28,14 +28,80 @@ void cambioProceso(uint32_t idPrograma){
 
 };
 
+void procesarSolicitudOperacionCPU(t_datos_hilo datosSockets){
 
-void procesarSolicitudOperacion(){
+	while(1) {
 
-	while(1){
+			int header = recibirHeader(datosSockets -> socketCliente);
+
+
+			/*switch(header){
+
+
+				case 8: //solicitarBytes
+				uint32_t pid;
+				int * largoPrograma;
+				uint32_t paginas_codigo;
+				char * programa;
+				recibirInicializacionPrograma(datosSockets -> listener, &pid, &largoPrograma, programa, &paginas_codigo);
+
+				//blabla bla recibir programa
+				//recibir lo demas
+				solicitarBytesDeUnaPag();
+				break;
+				case 9: //almacenarBytes
+				//blabla procesar bytes
+				//recibir lo demas
+				guardarBytesDeUnaPag();
+				break;
+
+				default:
+
+
+
+
+				break;
+				//vete de aqui hilo
+				//eclipse haceme el tp
+
+			}*/
+
+
+		}
+}
+
+
+
+void procesarSolicitudOperacionCPU(t_datos_hilo datosSockets){
+
+	while(1) {
+
+		int header = recibirHeader(datosSockets -> socketCliente);
+
+
+		switch(header){
+
+			case 8: //solicitarBytes
+			//recibir lo demas
+			solicitarBytesDeUnaPag();
+			break;
+			case 9: //almacenarBytes
+			//blabla procesar bytes
+			//recibir lo demas
+			guardarBytesDeUnaPag();
+			break;
+
+			default:
+			//se desconectó CPU o hubo problema de conexión
+			//chau hilo
+
+			break;
+
+
+		}
 		//recibirSolicitudes
 		//procesarlas
 		//send para responderle a CPUs
-		//en caso de que llegue un 0 kill el hilo
 
 	}
 
