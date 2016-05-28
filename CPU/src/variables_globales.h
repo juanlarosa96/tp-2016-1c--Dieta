eec89a26af1a8b279756ab7d12222423c2605a3e
Sofia Cortes <sofiavcortes@gmail.com>
2016-05-28 20:15:37 -0300

    Agrego variables globales en CPU

---
+++ CPU/src/variables_globales.h
@@ -0,0 +1,17 @@
+/*
+ * variables_globales.h
+ *
+ *  Created on: 28/5/2016
+ *      Author: utnso
+ */
+
+#ifndef VARIABLES_GLOBALES_H_
+#define VARIABLES_GLOBALES_H_
+
+#include <structs.h>
+
+int socketNucleo;
+
+t_pcb pcbRecibido;
+
+#endif /* VARIABLES_GLOBALES_H_ */
