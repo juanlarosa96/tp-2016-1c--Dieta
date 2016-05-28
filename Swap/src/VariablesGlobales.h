e15bd2541e32c91ae76f4645a84212d255098937
Fatta <lfatta2@hotmail.com>
2016-05-28 20:52:17 -0300

    comienzo inicializacion de programa Ansisop (y algunas otras cosa)

---
+++ Swap/src/VariablesGlobales.h
@@ -0,0 +1,15 @@
+/*
+ * VariablesGlobales.h
+ *
+ *  Created on: 28/5/2016
+ *      Author: utnso
+ */
+
+#ifndef VARIABLESGLOBALES_H_
+#define VARIABLESGLOBALES_H_
+
+int cantidadDeFrames;
+int sizePagina;
+int retardoCompactacion;
+
+#endif /* VARIABLESGLOBALES_H_ */
