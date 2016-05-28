fa2700ce730cb78149d5cad71c5bd907fc2f5bd5
Sofia Cortes <sofiavcortes@gmail.com>
2016-05-27 23:53:43 -0300

    Agrego .h de variables globales UMC

---
+++ UMC/src/variablesGlobalesUMC.h
@@ -0,0 +1,15 @@
+/*
+ * variablesGlobalesUMC.h
+ *
+ *  Created on: 27/5/2016
+ *      Author: utnso
+ */
+
+#ifndef VARIABLESGLOBALESUMC_H_
+#define VARIABLESGLOBALESUMC_H_
+
+t_list * listaFrames;
+t_list * listaProcesos;
+
+
+#endif /* VARIABLESGLOBALESUMC_H_ */
