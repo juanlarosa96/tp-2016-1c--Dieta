4a93d7d753a4cc23c69dccb1e7a677317480622c
Juan Larosa <juanlarosa96@gmail.com>
2016-05-30 19:52:04 -0300

    Agrego funcion de finalizarPrograma en funciones de Nucleo

--- Nucleo/src/funciones.c
+++ Nucleo/src/funciones.c
@@ -35,14 +35,23 @@ void manejarCPU(int socketCpu) {
 			while (!cambioProceso) {
 				if (recibirRespuestaCPU(socketCpu, &respuesta)) {
 					//Se desconecto el CPU
-					//finalizarProceso(siguientePcb);
+					finalizarProceso(siguientePcb);
 					desconectado = 1;
 					return;
 				}
 				switch (respuesta) {
 
 				case 99: //Fin programa
-					//finalizarProceso(siguientePcb);
+					finalizarProceso(siguientePcb);
+					int largoLista = list_size(listaConsolas), i;
+					for (i = 0; i < largoLista; i++) {
+						t_pcbConConsola * pcbBusqueda = (t_pcbConConsola) list_get(&listaConsolas, i);
+						if (pcbBusqueda->socketConsola == siguientePcb.socketConsola) {
+							t_pcbConConsola * pcbFinalizado = (t_pcbConConsola) list_remove(listaConsolas, i);
+							AgregarAProcesoColaFinalizados(*pcbFinalizado);
+							free(pcbFinalizado);
+						}
+					}
 					break;
 
 				case 100: //Fin quantum
@@ -143,5 +152,11 @@ int calcularPaginasCodigo(int largoPrograma) {
 int iniciarUnPrograma(int clienteUMC, t_pcb nuevoPcb, int largoPrograma, char * programa) {
 	enviarInicializacionPrograma(clienteUMC, nuevoPcb.pid, largoPrograma, programa, nuevoPcb.paginas_codigo);
 	return recibirRespuestaInicializacion(clienteUMC);
+
+}
+
+void finalizarProceso(t_pcbConConsola siguientePcb, int socketUMC) {
+	enviarFinalizacionProgramaUMC(socketUMC, siguientePcb.pcb.pid);
+	enviarFinalizacionProgramaConsola(siguientePcb.socketConsola);
 
 }
