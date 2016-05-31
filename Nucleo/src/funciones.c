0d1fa492d8d532514439d4bc6027c3d6daf01463
Juan Larosa <juanlarosa96@gmail.com>
2016-05-31 01:40:20 -0300

    Corrijo errores de casteo en funcion manejar CPU

--- Nucleo/src/funciones.c
+++ Nucleo/src/funciones.c
@@ -43,11 +43,11 @@ void manejarCPU(int socketCpu) {
 
 				case 99: //Fin programa
 					finalizarProceso(siguientePcb);
-					int largoLista = list_size(listaConsolas), i;
+					int largoLista = list_size(&listaConsolas), i;
 					for (i = 0; i < largoLista; i++) {
-						t_pcbConConsola * pcbBusqueda = (t_pcbConConsola) list_get(&listaConsolas, i);
+						t_pcbConConsola * pcbBusqueda = (t_pcbConConsola *) list_get(&listaConsolas, i);
 						if (pcbBusqueda->socketConsola == siguientePcb.socketConsola) {
-							t_pcbConConsola * pcbFinalizado = (t_pcbConConsola) list_remove(listaConsolas, i);
+							t_pcbConConsola * pcbFinalizado = (t_pcbConConsola *) list_remove(&listaConsolas, i);
 							AgregarAProcesoColaFinalizados(*pcbFinalizado);
 							free(pcbFinalizado);
 						}
