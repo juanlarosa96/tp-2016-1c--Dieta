face5d37b6f4ed86580a35a856172f807c7abca8
Sofia Cortes <sofiavcortes@gmail.com>
2016-06-09 16:21:57 -0300

    Agrego .c y .h para dump hexadecimal

---
+++ UMC/src/hexdump.h
@@ -0,0 +1,16 @@
+/*
+ * hexdump.h
+ *
+ *  Created on: 9/6/2016
+ *      Author: utnso
+ */
+
+#ifndef HEXDUMP_H_
+#define HEXDUMP_H_
+
+#include <stdio.h>
+#include <ctype.h>
+
+void hexdump(void *memoria, unsigned int len);
+
+#endif /* HEXDUMP_H_ */
