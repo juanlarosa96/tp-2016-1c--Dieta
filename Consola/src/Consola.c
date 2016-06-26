781d8236fb2a2f2fdca7e1f4c7caafcf8da0f345
Juan Larosa <juanlarosa96@gmail.com>
2016-06-26 18:17:04 -0300

    Corrijo envio de parametros a hilo de Consola

--- Consola/src/Consola.c
+++ Consola/src/Consola.c
@@ -1,7 +1,6 @@
 #include "funciones.h"
 #include "commons/log.h"
 
-
 int main(int argc, char **argv) {
 
 	t_config* config;
@@ -17,13 +16,12 @@ int main(int argc, char **argv) {
 	int PUERTO_NUCLEO = config_get_int_value(config, "PUERTO_NUCLEO");
 	char* IP_NUCLEO = config_get_string_value(config, "IP_NUCLEO");
 
-	char ruta[strlen(argv[1])+1];
+	char ruta[strlen(argv[1]) + 1];
 
 	strcpy(ruta, argv[1]); //Recibe ruta de programa ansisop
 
 	//Creo log para la consola
-	logger = log_create("Consola.log", "CONSOLA", 1,
-			log_level_from_string("INFO"));
+	logger = log_create("Consola.log", "CONSOLA", 1, log_level_from_string("INFO"));
 	char *texto;
 	texto = "info";
 
@@ -36,8 +34,6 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-
-
 	if (responderHandshake(socketNucleo, IDCONSOLA, IDNUCLEO)) {
 		log_error(logger, "Error en el handshake", texto);
 		return 1;
@@ -48,59 +44,55 @@ int main(int argc, char **argv) {
 	unsigned long largoPrograma;
 
 	FILE * archivo = fopen(ruta, "r");
-	    fseek(archivo, 0, SEEK_END);
-	    largoPrograma = (unsigned long)ftell(archivo);
-	    fseek(archivo, 0, SEEK_SET);
+	fseek(archivo, 0, SEEK_END);
+	largoPrograma = (unsigned long) ftell(archivo);
+	fseek(archivo, 0, SEEK_SET);
 
-	    char programaAnsisop[largoPrograma+1];
-	    int i;
+	char programaAnsisop[largoPrograma + 1];
+	int i;
 
-	    for(i = 0; i <largoPrograma;i++){
-	    	programaAnsisop[i] = fgetc(archivo);
-	    }
-	    fclose(archivo);
+	for (i = 0; i < largoPrograma; i++) {
+		programaAnsisop[i] = fgetc(archivo);
+	}
+	fclose(archivo);
 
-	    programaAnsisop[largoPrograma] = '\0';
+	programaAnsisop[largoPrograma] = '\0';
 
-	enviarProgramaAnsisop(socketNucleo,programaAnsisop,largoPrograma+1);
+	enviarProgramaAnsisop(socketNucleo, programaAnsisop, largoPrograma + 1);
 	log_info(logger, "Envió un mensaje a núcleo \n", texto);
 
 	pthread_t nuevoHilo;
 	pthread_attr_t attr;
+	int * socketParaThread = malloc(sizeof(int));
+	*socketParaThread = socketNucleo;
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-	pthread_create(&nuevoHilo, &attr, (void *) &interpreteComandos, (void *) &socketNucleo);
+	pthread_create(&nuevoHilo, &attr, (void *) &interpreteComandos, (void *) socketParaThread);
 	pthread_attr_destroy(&attr);
 	log_info(logger, "Se creó hilo de intérprete de comandos");
 
-	while(1){
+	while (1) {
 		int header = recibirHeader(socketNucleo);
 		if (header == resultadoEjecucion) {
-		/*int largoMensaje;
-		largoMensaje = recibirLargoResultadoDeEjecucionAnsisop(socketNucleo);
-		char mensajeDevuelto[largoMensaje];
-		recibirResultadoDeEjecucionAnsisop(socketNucleo,mensajeDevuelto,largoMensaje);*/
 			int largoTexto;
 			char *mensajeDevuelto;
-			recibirResultadoDeEjecucionAnsisop(socketNucleo,&mensajeDevuelto, &largoTexto);
-		log_info(logger, "Se recibio resultado de ejecucion");
-		log_info(logger, "Mensaje Recibido: %s \n", mensajeDevuelto);
+			recibirResultadoDeEjecucionAnsisop(socketNucleo, &mensajeDevuelto, &largoTexto);
+			log_info(logger, "Se recibio resultado de ejecucion");
+			log_info(logger, "Mensaje Recibido: %s \n", mensajeDevuelto);
 		}
 
-		if(header == finalizacionPrograma){
+		if (header == finalizacionPrograma) {
 			close(socketNucleo);
 			log_info(logger, "Programa finalizado", texto);
 			return EXIT_SUCCESS;
 
-			if(header <= 0){
-				log_error(logger, "Se desconecto el Nucleo");
-				return EXIT_SUCCESS;
-			}
-
 		}
 
+		if (header <= 0) {
+			log_error(logger, "Se desconecto el Nucleo");
+			return EXIT_SUCCESS;
+		}
 	}
-
 
 	log_destroy(logger);
 
